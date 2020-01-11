1e8bf361c51fb82b7da3dc1dbdc2d4b2462550f6
v_ajyzhong(钟建宇) <v_ajyzhong@tencent.com>
2020-01-11 19:54:07 +0800

    CardView

--- lib/main.dart
+++ lib/main.dart
@@ -1,108 +1,83 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+main() => runApp(App());
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class TextTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Text("Wow, Hello World!")
+    );
+  }
+}
+class App extends StatelessWidget {
+  // final TextDecoration textDecoration = TextDecoration.;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // 应用主题
-        //
-        // 使用 "flutter run" 运行应用. 你会看到一个 蓝色的 工具栏 
-        // 然后，不要退出这个 App, 尝试变更下面的 primarySwatch 为 Colors.green
-        // 然后 调用 "hot reload" ( 在你运行 flutter run 的命令行中 按下 r 键，
-        // 或者 在 flutter IDE 中 可以直接 保存保存你的更改 )
-        // 发现计数器 没有 被重新设置为 0，应用也没有重新启动
-        primarySwatch: Colors.green
-      ),
-      home: MyHomePage(title: 'Flutter 示例的 Home 页面'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("我的 Flutter Demo")
+        ),
+        body: HomePage()
+      )
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // 这个 widget 你应用的主页面. 它是有状态的, 
-  // 意思是它有一个 状态对象( 下面定义的 ) 来包含一些影响它外观的字段
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomePage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    const data = [
+      ["标题1", "描述1描述1描述1描述1描述1描述1", "http://pic1.win4000.com/wallpaper/8/546b16a7cfa03.jpg"],
+      ["标题1", "描述1描述1描述1描述1描述1描述1", "http://pic1.win4000.com/wallpaper/8/546b16a7cfa03.jpg"],
+      ["标题1", "描述1描述1描述1描述1描述1描述1", "http://pic1.win4000.com/wallpaper/8/546b16a7cfa03.jpg"],
+    ];
+    final cardViewList = List<CardView>.from(
+      data.map((data) => CardView(data[0], data[1], data[2]))
+    );
+    return Container(
+      child: ListView(
+        children: [
+          ...cardViewList,
+          SizedBox(height: 8)
+        ]
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class CardView extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String imageurl;
 
-  _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  CardView(this.title, this.subtitle, this.imageurl);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      padding: EdgeInsets.all(8),
+      margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+      decoration: BoxDecoration(
+        border: Border.all(width: 1, color: Colors.grey)
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '你已经点击按钮多次了:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display4,
-            ),
-          ],
-        ),
+      child: Column(
+        children: <Widget>[
+          Text(
+            title,
+            style: TextStyle( fontSize: 30 )
+          ),
+          SizedBox(height: 8),
+          Text(
+            subtitle,
+            style: TextStyle(color: Colors.grey, fontSize: 18 )
+          ),
+          Image.network(imageurl)
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+
 }
